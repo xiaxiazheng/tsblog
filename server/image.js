b337686b04f3662f2d6c0d86591a11636d885538
zyb <13075218875@163.com>
2018-10-25 16:13:40 +0800

    小bug

--- server/image.js
+++ server/image.js
@@ -54,7 +54,7 @@ exports.saveWallImg = async (ctx) => {
 // 树详细内容的，treeCont
 exports.saveTreeContImg = async (ctx) => {
   // console.log(ctx.req.file);  /* 上传的文件信息 */
-  let filename = Common.getImageName(ctx.req.file.originalname, (req.body.c_id + '.' + Common.getRandomNum()));
+  let filename = Common.getImageName(ctx.req.file.originalname, (ctx.request.body.c_id + '.' + Common.getRandomNum()));
   let saveTo = __dirname + "/img/treecont/" + filename; /* 这里要注意，因为这个文件已经在server里了，所以这里的__dirname是有server的 */
   
   try {
@@ -64,7 +64,7 @@ exports.saveTreeContImg = async (ctx) => {
 
     let time = Common.getNowFormatDate();
     let sql = "UPDATE cont SET filename=?, mTime=? WHERE c_id=? && sort=?";
-    let array = [filename, time, req.body.c_id, req.body.sort];
+    let array = [filename, time, ctx.request.body.c_id, ctx.request.body.sort];
     await query(sql, array);
   } catch(e) {
     console.log(e);
