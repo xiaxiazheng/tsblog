c51455beafa888115d25e7aae9f026b96e761c81
zyb <13075218875@163.com>
2018-12-05 15:22:48 +0800

    修复

--- src/components/TreeMain.vue
+++ src/components/TreeMain.vue
@@ -23,7 +23,7 @@
         <div class="resultNo" v-if="searchkeyword && !isSeaching && (!nowContList || nowContList.length === 0)">暂无搜索结果</div>
         <!-- 树首页的默认展示 -->
         <div class="resultTitle" v-if="!searchkeyword">
-          最近修改的五个节点：
+          最近更新的五个节点：
         </div>
         <!-- 结果列表 -->
         <div class="searchResult" v-for="(item, index) in nowContList" :key="index">
