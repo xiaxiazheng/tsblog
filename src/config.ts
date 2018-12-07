e26936ba03c5c91cbd1dbd5a69c380ecdca5ae5d
zyb <13075218875@163.com>
2018-12-07 16:40:00 +0800

    通过给 npm run build 设置 --mode 区分两种线上环境

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,21 @@
 // 前端访问路径及所在环境
 let url: string = "";
 let env: string = "";
-if (process.env.NODE_ENV.match('productionPig')) {
-  url = 'http://123.207.5.131:518';
-  env = "productionPig";
-} else if (process.env.NODE_ENV.match('production')) {
-  url = 'http://123.207.5.131:3000';
-  env = "production";
-} else {
+if (process.env.NODE_ENV.match('production')) {
+  if  (process.env.VUE_APP_TITLE.match('hyp')) {
+    url = 'http://123.207.5.131:518';
+    env = "productionPig";    
+  }
+  if (process.env.VUE_APP_TITLE.match('zyb')) {
+    url = 'http://123.207.5.131:3000';
+    env = "production";
+  }
+}
+if (process.env.NODE_ENV.match('development')) {
   url = 'http://localhost:3000';
   env = "development";
 }
+console.log('VUE_APP_TITLE: ', process.env.VUE_APP_TITLE);
 console.log(process.env.NODE_ENV, url);
 export const baseImgUrl = url;
 export const baseUrl = `${url}/back`;
